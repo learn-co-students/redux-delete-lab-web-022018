a233ee7709d438140ed9eea7c87b393cdb321d7e
github username <github email address>
2018-05-11 14:47:17 -0400

    Done.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import BandInput from './components/BandInput';
 import Bands from './components/Bands';
 
+
+
 export class App extends Component {
   render() {
     return (
